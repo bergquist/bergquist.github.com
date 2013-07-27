3e9092200ce3ef8fb111127caceb77dc8a348fb1
Carl Bergquist <carl.bergquist@gmail.com>
2013-07-27 09:33:36 +0200

    Now using grunt for building

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,125 +1,118 @@
 'use strict';
 var liveReloadPort = 35729;
 var lrSnippet = require('connect-livereload')({
-    port: liveReloadPort
+  port: liveReloadPort
 });
 var mountFolder = function(connect, dir) {
-    return connect.static(require('path').resolve(dir));
+  return connect.static(require('path').resolve(dir));
 };
 var yeomanConfig = {
-    app: 'app',
-    dist: 'dist'
+  app: 'app',
+  dist: 'dist'
 };
 
 module.exports = function(grunt) {
-    grunt.initConfig({
-        yeoman: yeomanConfig,
+  grunt.initConfig({
+    yeoman: yeomanConfig,
 
-        watch: {
-            livereload: {
-                options: {
-                    livereload: liveReloadPort
-                },
-                files: [
-                        '.jekyll/**/*.html',
-                        '.tmp/css/**/*.css',
-                        '{.tmp,<%= yeoman.app %>}/<%= js %>/**/*.js',
-                        '<%= yeoman.app %>/image/**/*.{gif,jpg,jpeg,png,svg,webp}'
-                ]
-            },
-            compass: {
-                files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-                tasks: ['compass:server']
-            }
+    watch: {
+      livereload: {
+        options: {
+          livereload: liveReloadPort
         },
-        open: {
-            server: {
-                path: 'http://localhost:<%= connect.options.port %>'
-            }
-        },
-        compass: {
-            options: {
-                sassDir: 'css',
-                cssDir: '.tmp/styles'//,
-                /*
-                generatedImagesDir: '.tmp/images/generated',
-                imagesDir: '<%= yeoman.app %>/images',
-                javascriptsDir: '<%= yeoman.app %>/scripts',
-                fontsDir: '<%= yeoman.app %>/styles/fonts',
-                importPath: '<%= yeoman.app %>/bower_components',
-                httpImagesPath: '/images',
-                httpGeneratedImagesPath: '/images/generated',
-                httpFontsPath: '/styles/fonts',
-                relativeAssets: false
-                */
-            },
-            dist: {},
-            server: {
-                options: {
-                    debugInfo: true
-                }
-            }
-        },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            all: [
-                    'Gruntfile.js'
-            ],
-            report: [
-                    'Gruntfile.js',
-                    '!<%= yeoman.app %>/js/vendor/**/*'
-            ]
-        },
-        csslint: {
-            options: {
-                csslintrc: '.csslintrc'
-            },
-            report: {
-                src: ['{.tmp,<%= yeoman.app %>}/css/**/*.css']
-            }
-        },
-        cssmin: {
-            dist: {
-                files: {
-                    'style.min.css': [
-                        '.tmp/styles/{,*/}*.css'
-                    ]
-                }
-            }
-        },
-        usemin: {
-            options: {
-                basedir: '<%= yeoman.dist %>',
-                dirs: ['<%= yeoman.dist %>/**/*']
-            },
-            html: ['<%= yeoman.dist %>/**/*.html'],
-            css: ['<%= yeoman.dist %>/css/**/*.css']
-        },
-        rev: {
-            options: {
-                length: 4
-            },
-            dist: {
-                files: {
-                    src: [
-                            '<%= yeoman.dist %>/js/**/*.js',
-                            '<%= yeoman.dist %>/css/**/*.css',
-                            '<%= yeoman.dist %>/image/**/*.{gif,jpg,jpeg,png,svg,webp}',
-                            '<%= yeoman.dist %>/fonts/**/*.{eot*,svg,ttf,woff}'
-                    ]
-                }
-            }
+        files: [
+            '.jekyll/**/*.html',
+            '.tmp/css/**/*.css',
+            '{.tmp,<%= yeoman.app %>}/<%= js %>/**/*.js',
+            '<%= yeoman.app %>/image/**/*.{gif,jpg,jpeg,png,svg,webp}'
+        ]
+      },
+      compass: {
+        files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['compass:server']
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        // Change hostname to null to access the server from outside.
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, '_site'),
+              mountFolder(connect, yeomanConfig.app)];
+          }
         }
-    });
+      },
+      test: {
+        options: {
+          middleware: function(connect) {
+            return [
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'test')];
+          }
+        }
+      },
+      dist: {
+        options: {
+          middleware: function(connect) {
+            return [
+              mountFolder(connect, yeomanConfig.dist)];
+          }
+        }
+      }
+    },
+    clean: {
+      dist: {
+        files: [{
+            dot: true,
+            src: [
+                '.tmp',
+                '<%= yeoman.dist %>/*',
+                '!<%= yeoman.dist %>/.git*'
+            ]
+          }
+        ]
+      },
+      server: ['.tmp', '.jekyll']
+    },
+    open: {
+      server: {
+        path: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          'style.min.css': [
+              '.tmp/{,*/}*.css'
+          ]
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['style.scss'],
+          dest: '.tmp/',
+          ext: '.css'
+        }]
+      }
+    }
+  });
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    grunt.registerTask('default', [
-        'jshint:all',
-        'csslint',
-        'compass',
-        'cssmin'
-    ]);
+  grunt.registerTask('default', [
+    'clean',
+    'sass',
+    'cssmin'
+  ]);
 };
